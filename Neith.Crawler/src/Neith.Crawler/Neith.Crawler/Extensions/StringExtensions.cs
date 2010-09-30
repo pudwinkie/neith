9e1a6aa09ab4f975c36143e1983cf8c32b4602b1
lucia.neith.vb <lucia.neith.vb@localhost>
2010-09-30 16:16:00 +0900

    ???????

---
+++ Neith.Crawler/src/Neith.Crawler/Neith.Crawler/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string FormatText(this string format, object arg1) { return string.Format(format, arg1); }
+        public static string FormatText(this string format, object arg1, object arg2) { return string.Format(format, arg1, arg2); }
+        public static string FormatText(this string format, object arg1, object arg2, object arg3) { return string.Format(format, arg1, arg2, arg3); }
+        public static string FormatText(this string format, params object[] args) { return string.Format(format, args); }
+    }
+}
