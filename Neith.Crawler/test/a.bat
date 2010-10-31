9e47579b8b602d35db2cd4d3bc71b10b572f9a54
lucia.neith.vb <lucia.neith.vb@localhost>
2010-11-01 00:17:22 +0900

    ????????

---
+++ Neith.Crawler/test/a.bat
@@ -0,0 +1,16 @@
+ren "Leves    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"                               levelist.html
+ren "NPC／モンスター    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"                     moblist.html
+ren "Quest List    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"                          questlist.html
+ren "Zone List    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"                           zonelist.html
+ren "ある始まり    クエスト    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"              quest.html
+ren "アイテム    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"                            itemlist.html
+ren "アッシュ板    レシピ    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"                recipe.html
+ren "ウィーヴルの防除    ギルドリーヴ    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"    leve.html
+ren "ウェザードグラディウス    アイテム    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"  item1.html
+ren "ウェザードローブ    アイテム    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"        item2.html
+ren "カッパープレートレット    レシピ    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"    recipe.html
+ren "フィンヘームル    NPC／モンスター    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"   mob.html
+ren "ベニザリガニ    アイテム    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"            item3.html
+ren "リムサ・ロミンサ    エリア    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"          zone.html
+ren "レシピ    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"                              recipelist.html
+ren "採集    Final Fantasy XIV (FFXIV, FF14)    ZAM.htm"                                harvestlist.html
