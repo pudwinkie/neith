3410aa2cf1eb96918b0db919a676e1e8577972f6
lucia.neith.vb <lucia.neith.vb@localhost>
2010-11-23 16:42:21 +0900

    ???????

---
+++ Neith.Signpost/src/UnitTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTest
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Neith.Util.UnitTest.ConsoleRunner.Run(args);
+        }
+    }
+}
