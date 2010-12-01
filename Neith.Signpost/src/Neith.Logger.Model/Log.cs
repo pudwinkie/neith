14f9dfbda1dab1dfcdac5d6de7f0a6753b358e48
lucia.neith.vb <lucia.neith.vb@localhost>
2010-12-01 18:47:34 +0900

    ??

--- Neith.Signpost/src/Neith.Logger.Model/Log.cs
+++ Neith.Signpost/src/Neith.Logger.Model/Log.cs
@@ -15,65 +15,89 @@ namespace Neith.Logger.Model
         {
             get
             {
-                return new DateTimeOffset(
+                if (_timestamp == DateTimeOffset.MinValue) {
+                    _timestamp = new DateTimeOffset(
                     DateTime.FromBinary(TimestampBinary),
                     TimeSpan.FromTicks(TimestampOffset));
+                }
+                return _timestamp;
             }
             set
             {
+                _timestamp = value;
                 TimestampBinary = value.DateTime.ToBinary();
                 TimestampOffset = value.Offset.Ticks;
             }
         }
-        [ProtoMember(1, IsRequired = true)]
+        private DateTimeOffset _timestamp = DateTimeOffset.MinValue;
+
+        [ProtoMember(1)]
         private long TimestampBinary { get; set; }
-        [ProtoMember(2, IsRequired = true)]
+        [ProtoMember(2)]
         private long TimestampOffset { get; set; }
 
+
         /// <summary>ログの収集モジュール</summary>
-        [ProtoMember(3, IsRequired = true)]
+        [ProtoMember(3)]
         public string Collector { get; set; }
 
         /// <summary>ホスト</summary>
-        [ProtoMember(4, IsRequired = true)]
+        [ProtoMember(4)]
         public string Host { get; set; }
 
+
         /// <summary>プロセスID</summary>
-        [ProtoMember(5, IsRequired = true)]
+        [ProtoMember(5)]
         public int Pid { get; set; }
 
+
         /// <summary>ログの取得元アプリ</summary>
-        [ProtoMember(6, IsRequired = true)]
+        [ProtoMember(6)]
         public string Application { get; set; }
 
+
         /// <summary>ログの取得元ドメイン</summary>
-        [ProtoMember(7, IsRequired = true)]
+        [ProtoMember(7)]
         public string Domain { get; set; }
 
-        /// <summary>ログの取得元ドメイン</summary>
-        [ProtoMember(8, IsRequired = true)]
+
+        /// <summary>ログの取得元ユーザ</summary>
+        [ProtoMember(8)]
         public string User { get; set; }
 
-        /// <summary>ログの分析モジュール</summary>
-        [ProtoMember(9, IsRequired = true)]
-        public string Analyzer { get; set; }
 
         /// <summary>ログのカテゴリ</summary>
-        [ProtoMember(10, IsRequired = true)]
+        [ProtoMember(9)]
         public string Category { get; set; }
 
+
         /// <summary>ログのタイプ</summary>
-        [ProtoMember(11, IsRequired = true)]
+        [ProtoMember(10)]
         public string Type { get; set; }
+
 
         /// <summary>ログの優先度</summary>
         public TraceEventType Priority { get; set; }
-        [ProtoMember(12, IsRequired = true)]
+        [ProtoMember(11)]
         private int _priority
         {
             get { return (int)Priority; }
             set { Priority = (TraceEventType)value; }
         }
+
+
+        /// <summary>ログのメッセージ</summary>
+        [ProtoMember(12)]
+        public string Message { get; set; }
+
+
+
+        /// <summary>ログの分析モジュール</summary>
+        [ProtoMember(13)]
+        public string Analyzer { get; set; }
+
+
+
 
 
     }
