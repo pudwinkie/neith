a7bf14bddd643a115544f03f8f1af38a8b3127e9
lucia.neith.vb <lucia.neith.vb@localhost>
2010-12-02 18:57:07 +0900

    ????????

---
+++ Neith.Signpost/src/Neith.Logger.Test/LogStoreTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Neith.Logger;
+using Neith.Logger.Model;
+
+namespace Neith.Logger.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LogStoreTest
+    {
+        [Test]
+        public void WriteTest1()
+        {
+            var store = LogStore.Instance;
+            try {
+                var item = new Log();
+                item.Timestamp = DateTimeOffset.Now;
+                item.Host = "Host";
+                item.Pid = 123;
+                item.Application = "ろぐしゅつりょくテストなのよー";
+                store.Store(item);
+            }
+            finally {
+                LogStore.StoreClose();
+            }
+        }
+
+    }
+}
