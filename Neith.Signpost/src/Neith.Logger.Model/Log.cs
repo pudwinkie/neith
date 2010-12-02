7156b93257948f2641c96cc27e8a8dcd51566b41
lucia.neith.vb <lucia.neith.vb@localhost>
2010-12-02 11:57:39 +0900

    ?????????

--- Neith.Signpost/src/Neith.Logger.Model/Log.cs
+++ Neith.Signpost/src/Neith.Logger.Model/Log.cs
@@ -11,91 +11,90 @@ namespace Neith.Logger.Model
     public class Log
     {
         /// <summary>タイムスタンプ</summary>
-        public DateTimeOffset Timestamp
-        {
-            get
-            {
-                if (_timestamp == DateTimeOffset.MinValue) {
-                    _timestamp = new DateTimeOffset(
-                    DateTime.FromBinary(TimestampBinary),
-                    TimeSpan.FromTicks(TimestampOffset));
-                }
-                return _timestamp;
-            }
-            set
-            {
-                _timestamp = value;
-                TimestampBinary = value.DateTime.ToBinary();
-                TimestampOffset = value.Offset.Ticks;
-            }
-        }
-        private DateTimeOffset _timestamp = DateTimeOffset.MinValue;
+        public DateTimeOffset Timestamp{get;set;}
+
+        /// <summary>タイムスタンプ(UTC時刻)</summary>
+        public DateTime TimestampUTC { get { return Timestamp.UtcDateTime; } }
 
         [ProtoMember(1)]
-        private long TimestampBinary { get; set; }
-        [ProtoMember(2)]
-        private long TimestampOffset { get; set; }
+        private long TimestampBinary
+        {
+            get { return TimestampUTC.ToBinary(); }
+            set { Timestamp = new DateTimeOffset(DateTime.FromBinary(value).ToLocalTime()); }
+        }
 
 
         /// <summary>ログの収集モジュール</summary>
-        [ProtoMember(3)]
+        [ProtoMember(2)]
         public string Collector { get; set; }
 
+
         /// <summary>ホスト</summary>
-        [ProtoMember(4)]
+        [ProtoMember(3)]
         public string Host { get; set; }
 
 
         /// <summary>プロセスID</summary>
-        [ProtoMember(5)]
+        [ProtoMember(4)]
         public int Pid { get; set; }
 
 
         /// <summary>ログの取得元アプリ</summary>
-        [ProtoMember(6)]
+        [ProtoMember(5)]
         public string Application { get; set; }
 
 
         /// <summary>ログの取得元ドメイン</summary>
-        [ProtoMember(7)]
+        [ProtoMember(6)]
         public string Domain { get; set; }
 
 
         /// <summary>ログの取得元ユーザ</summary>
-        [ProtoMember(8)]
+        [ProtoMember(7)]
         public string User { get; set; }
 
 
         /// <summary>ログのカテゴリ</summary>
-        [ProtoMember(9)]
+        [ProtoMember(8)]
         public string Category { get; set; }
 
 
         /// <summary>ログのタイプ</summary>
-        [ProtoMember(10)]
+        [ProtoMember(9)]
         public string Type { get; set; }
 
 
         /// <summary>ログの優先度</summary>
-        public TraceEventType Priority { get; set; }
+        [ProtoMember(10)]
+        public LogPriority Priority { get; set; }
+
+
+        /// <summary>行為を行った者</summary>
         [ProtoMember(11)]
-        private int _priority
-        {
-            get { return (int)Priority; }
-            set { Priority = (TraceEventType)value; }
-        }
+        public string Actor { get; set; }
+
+
+        /// <summary>行為の対象者</summary>
+        [ProtoMember(12)]
+        public string Target { get; set; }
 
 
         /// <summary>ログのメッセージ</summary>
-        [ProtoMember(12)]
+        [ProtoMember(13)]
         public string Message { get; set; }
 
 
-
         /// <summary>ログの分析モジュール</summary>
-        [ProtoMember(13)]
+        [ProtoMember(14)]
         public string Analyzer { get; set; }
 
+
+        /// <summary>アイコン画像URL</summary>
+        [ProtoMember(15)]
+        public string Icon { get; set; }
+
+
+        // 14,15は現在のところ予約。かならず使うようなものがあれば追加する。
 
 
 
