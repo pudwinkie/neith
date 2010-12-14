ba96e3fd0a88c86633ca6dbec295ef6f80e1e138
lucia.neith.vb <lucia.neith.vb@localhost>
2010-12-14 17:32:03 +0900

    ??

---
+++ Neith.Signpost/src/Neith.Logger.Test/API.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows;
+
+namespace Neith.Logger.Test
+{
+    public static class API
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+
+            public int X { get { return left; } }
+            public int Y { get { return top; } }
+            public int Width { get { return right - left; } }
+            public int Height { get { return bottom - top; } }
+            public Int32Rect ToInt32Rect()
+            {
+                return new Int32Rect(X, Y, Width, Height);
+            }
+        }
+
+        [DllImport("User32.Dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowRect(
+               IntPtr hWnd,      // ウィンドウのハンドル
+               out RECT rect   // ウィンドウの座標値
+               );
+
+        public static Int32Rect GetWindowRect(this Process process)
+        {
+            var rect = new RECT();
+            if (GetWindowRect(process.MainWindowHandle, out rect) == 0) {
+                var hResult = Marshal.GetHRForLastWin32Error();
+                Marshal.ThrowExceptionForHR(hResult);
+            }
+            return rect.ToInt32Rect();
+        }
+
+    }
+}
---
+++ Neith.Signpost/src/Neith.Logger.Test/EtcTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Neith.Logger.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class EtcTest
+    {
+        [Test]
+        public void Is64()
+        {
+            // このソフトは64bitで確実に動作することを確認するために
+            // ユニットテストに64試験を組み込みます。
+            Assert.IsTrue(Environment.Is64BitProcess);
+        }
+
+        [Test]
+        public void AllProcessWindows()
+        {
+            var q1 = from p in Process.GetProcesses()
+                     let Name = p.ProcessName
+                     let IsWindow = (p.MainWindowHandle != IntPtr.Zero)
+                     let WinName = p.MainWindowTitle
+                     select new { Process = p, Name, IsWindow, WinName };
+            var q2 = q1.ToArray();
+
+            foreach (var a in q2) {
+                var mes = string.Format("[{0}]", a.Name);
+                if (a.IsWindow == true) mes += string.Format("  WIN[{0}]", a.WinName);
+                Debug.WriteLine(mes);
+            }
+
+            var q3 = from p in q2.Where(a => a.IsWindow)
+                     let Rect = p.Process.GetWindowRect()
+                     select new { p.WinName, Rect };
+
+            foreach (var a in q3) {
+                Debug.WriteLine(a);
+            }
+
+
+        }
+    }
+}
