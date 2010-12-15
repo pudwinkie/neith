4bf383702ae7b3a13651bd4520c6603a7e51eda6
dot.station <dot.station@localhost>
2010-12-16 00:56:55 +0900

    ?????????

---
+++ Neith.Signpost/src/FFXIVRuby/FFXIVProcess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace FFXIVRuby
+{
+    public class FFXIVProcess
+    {
+        // Fields
+        private Memory memory;
+        private Process proc;
+
+        // Methods
+        public FFXIVProcess(Process p)
+        {
+            this.proc = p;
+            this.memory = new Memory();
+        }
+
+        public Memory.MEMORY_BASIC_INFORMATION[] GetMemoryBasicInfos()
+        {
+            return memory.GetMemoryInfos(proc);
+        }
+
+        public byte[] ReadBytes(int address, int size)
+        {
+            return FFXIVMemoryProvidor.ReadProcessMemory(this.proc.Handle, address, size);
+        }
+
+        public int ReadInt32(int address)
+        {
+            byte[] buffer = FFXIVMemoryProvidor.ReadProcessMemory(this.proc.Handle, address, 4);
+            return (((buffer[0] + (buffer[1] * 0x100)) + (buffer[2] * 0x10000)) + (buffer[3] * 0x1000000));
+        }
+    }
+
+ 
+
+}
---
+++ Neith.Signpost/src/FFXIVRuby/FFXIVRuby.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace FFXIVRuby
+{
+    class FFXIVRuby
+    {
+        // Fields
+        private Process _proc;
+        private static string downloads_folder = "downloads";
+        private static string log_folder = "log";
+        private static string login_folder = "login";
+        private static string mcro_file = "mcr0";
+        private static string screenshots_folder = "screenshots";
+        private static string ui_file = "ui";
+        private static string user_folder = "user";
+        private static string Zero_folder = "00000000";
+
+        // Methods
+        public static FFXIVRuby Create()
+        {
+            var ruby = new FFXIVRuby();
+            ruby._proc = FFXIVMemoryProvidor.GetFFXIVGameProcess();
+            return ruby;
+        }
+
+        // Properties
+        public static string FFXIDownloadsFolderPath
+        {
+            get
+            {
+                return Path.Combine(MyGamesFFXIV, downloads_folder);
+            }
+        }
+
+        public static string FFXILoginFolderPath
+        {
+            get
+            {
+                return Path.Combine(MyGamesFFXIV, login_folder);
+            }
+        }
+
+        public static string FFXIScreenshotsFolderPath
+        {
+            get
+            {
+                return Path.Combine(MyGamesFFXIV, screenshots_folder);
+            }
+        }
+
+        public static string FFXIUserFolderPath
+        {
+            get
+            {
+                return Path.Combine(MyGamesFFXIV, user_folder);
+            }
+        }
+
+        private static string MyGamesFFXIV
+        {
+            get
+            {
+                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), @"My Games\FINAL FANTASY XIV");
+            }
+        }
+    }
+}
