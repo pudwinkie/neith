a1851e02b9c6300d5bdb1f61f552c4a76f8b5d06
lucia.neith.vb <lucia.neith.vb@localhost>
2011-05-30 17:55:21 +0900

    ????????

--- Neith.Signpost/src/Neith.Logger.Test/SQLTest.cs
+++ Neith.Signpost/src/Neith.Logger.Test/SQLTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if false
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
@@ -55,3 +56,4 @@ namespace Neith.Logger.Test
 
     }
 }
+#endif
