5744f0d06b31a038bf8a2d553761ae87927a27b2
lucia.neith.vb <lucia.neith.vb@localhost>
2011-06-03 11:55:56 +0900

    ??ViewModel???

---
+++ Neith.Signpost2/src/Neith.Signpost/Converters/StringFormatConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Neith.Signpost.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(string)) throw new ArgumentException("string型への変換以外はサポートしません");
+            var format = parameter as string;
+            if (format == null) throw new ArgumentException("パラメータがstring.Format引数ではありません");
+            format = "{0:" + format + "}";
+            return string.Format(format, value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Neith.Signpost2/src/Neith.Util/ObjectUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neith.Util
+{
+    /// <summary>
+    /// オブジェクト操作ユーティリティ。
+    /// </summary>
+    public static class ObjectUtil
+    {
+        /// <summary>
+        /// 値型の２値を入れ替えます。
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+        }
+
+        /// <summary>
+        /// Disposeを行い、変数を初期化します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        public static void CheckDispose<T>(ref T obj)
+            where T : IDisposable
+        {
+            if (obj == null) return;
+            obj.Dispose();
+            obj = default(T);
+        }
+
+    }
+}
