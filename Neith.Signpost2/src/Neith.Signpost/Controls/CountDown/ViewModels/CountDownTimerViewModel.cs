8c72de5dcb34283e1b375ae5f9819b8957ff7f54
lucia.neith.vb <lucia.neith.vb@localhost>
2011-06-06 17:52:18 +0900

    ????????????????

---
+++ Neith.Signpost2/src/Neith.Signpost/Controls/CountDown/ViewModels/CountDownTimerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace Neith.Signpost
+{
+    public class CountDownTimerViewModel : INotifyPropertyChanged
+    {
+        public CountDownTimerViewModel()
+        {
+            
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        #endregion
+    }
+}
