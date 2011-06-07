251f97c0f4da9280c3730d9a68911685fbee6ba4
lucia.neith.vb <lucia.neith.vb@localhost>
2011-06-07 18:13:46 +0900

    ??????????????

---
+++ Neith.Signpost2/src/Neith.Signpost/Converters/ToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Neith.Signpost.Converters
+{
+    public class ToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(Visibility)) throw new ArgumentException("Visibility型への変換以外はサポートしません");
+            if (value is bool) {
+                var flag = (bool)value;
+                return flag ? Visibility.Visible : Visibility.Hidden;
+            }
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
