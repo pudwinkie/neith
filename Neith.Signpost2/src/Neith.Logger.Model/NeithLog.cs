dd41613e062dfc80ab5dc37a58ad7220cc46e982
lucia.neith.vb <lucia.neith.vb@localhost>
2011-07-26 18:16:57 +0900

    ??

--- Neith.Signpost2/src/Neith.Logger.Model/NeithLog.cs
+++ Neith.Signpost2/src/Neith.Logger.Model/NeithLog.cs
@@ -4,82 +4,65 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
-using ReactiveUI;
+using Wintellect.Sterling;
 
 namespace Neith.Logger.Model
 {
     [Serializable]
-    public class NeithLog : ReactiveValidatedObject, IEquatable<NeithLog>
+    public class NeithLog : IEquatable<NeithLog>
     {
         #region フィールド
         /// <summary>タイムスタンプ(UTC時刻、IDを兼ねるためUnique保証)</summary>
         [Key]
-        public DateTimeOffset UtcTime { get { return _UtcTime; } set { this.RaiseAndSetIfChanged(a => a.UtcTime, value); } }
-        private DateTimeOffset _UtcTime = default(DateTimeOffset);
+        public DateTimeOffset UtcTime;
 
         /// <summary>ログの収集モジュール</summary>
-        public string Collector { get { return _Collector; } set { this.RaiseAndSetIfChanged(a => a.Collector, value); } }
-        private string _Collector = default(string);
+        public string Collector;
 
         /// <summary>ホスト</summary>
-        public string Host { get { return _Host; } set { this.RaiseAndSetIfChanged(a => a.Host, value); } }
-        private string _Host = default(string);
+        public string Host;
 
         /// <summary>プロセスID</summary>
-        public int Pid { get { return _Pid; } set { this.RaiseAndSetIfChanged(a => a.Pid, value); } }
-        private int _Pid = default(int);
+        public int Pid;
 
         /// <summary>ログの取得元アプリ</summary>
-        public string Application { get { return _Application; } set { this.RaiseAndSetIfChanged(a => a.Application, value); } }
-        private string _Application = default(string);
+        public string Application;
 
         /// <summary>ログの取得元ドメイン</summary>
-        public string Domain { get { return _Domain; } set { this.RaiseAndSetIfChanged(a => a.Domain, value); } }
-        private string _Domain = default(string);
+        public string Domain;
 
         /// <summary>ログの取得元ユーザ</summary>
-        public string User { get { return _User; } set { this.RaiseAndSetIfChanged(a => a.User, value); } }
-        private string _User = default(string);
+        public string User;
 
         /// <summary>ログ内容</summary>
-        public string LogText { get { return _LogText; } set { this.RaiseAndSetIfChanged(a => a.LogText, value); } }
-        private string _LogText = default(string);
+        public string LogText;
 
         /// <summary>ログの分析モジュール</summary>
-        public string Analyzer { get { return _Analyzer; } set { this.RaiseAndSetIfChanged(a => a.Analyzer, value); } }
-        private string _Analyzer = default(string);
+        public string Analyzer;
 
         /// <summary>WindowHandle</summary>
-        public IntPtr HWnd { get { return _HWnd; } set { this.RaiseAndSetIfChanged(a => a.HWnd, value); } }
-        private IntPtr _HWnd = default(IntPtr);
+        public IntPtr HWnd;
 
         /// <summary>ログのカテゴリ</summary>
-        public string Category { get { return _Category; } set { this.RaiseAndSetIfChanged(a => a.Category, value); } }
-        private string _Category = default(string);
+        public string Category;
 
         /// <summary>ログのタイプ</summary>
-        public string Type { get { return _Type; } set { this.RaiseAndSetIfChanged(a => a.Type, value); } }
-        private string _Type = default(string);
+        public string Type;
 
         /// <summary>ログの優先度</summary>
-        public NeithLogPriority Priority { get { return _Priority; } set { this.RaiseAndSetIfChanged(a => a.Priority, value); } }
-        private NeithLogPriority _Priority = default(NeithLogPriority);
+        public NeithLogPriority Priority;
 
         /// <summary>行為の実行者</summary>
-        public string Actor { get { return _Actor; } set { this.RaiseAndSetIfChanged(a => a.Actor, value); } }
-        private string _Actor = default(string);
+        public string Actor;
 
         /// <summary>行為の対象</summary>
-        public string Target { get { return _Target; } set { this.RaiseAndSetIfChanged(a => a.Target, value); } }
-        private string _Target = default(string);
+        public string Target;
 
         /// <summary>解析結果メッセージ</summary>
-        public string Message { get { return _Message; } set { this.RaiseAndSetIfChanged(a => a.Message, value); } }
-        private string _Message = default(string);
+        public string Message;
 
         /// <summary>アイコン画像URL</summary>
-        public string Icon { get { return _Icon; } set { this.RaiseAndSetIfChanged(a => a.Icon, value); } }
-        private string _Icon = default(string);
+        public string Icon;
 
         #endregion
         #region メソッド
