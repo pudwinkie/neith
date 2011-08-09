2e36721d8a744466a318ca1109b6b9fefb61e66b
lucia.neith.vb <lucia.neith.vb@localhost>
2011-08-09 18:24:01 +0900

    ????????

--- Neith.Signpost2/src/Neith.Logger.Model/NeithNotificationRec.cs
+++ Neith.Signpost2/src/Neith.Logger.Model/NeithNotificationRec.cs
@@ -12,7 +12,7 @@ using Wintellect.Sterling;
 namespace Neith.Logger.Model
 {
     [Serializable, DataContract]
-    public class NeithNotificationRec : IEquatable<NeithNotificationRec>
+    public class NeithNotificationRec
     {
         #region フィールド(Signpost KEY)
         /// <summary>受付時刻(UTC時刻、IDを兼ねるためUnique保証)</summary>
@@ -84,20 +84,14 @@ namespace Neith.Logger.Model
         /// <summary>イベント発生時刻</summary>
         public DateTimeOffset EventTime;
 
-        /// <summary>プロセスID</summary>
-        public int Pid;
-
-        /// <summary>ログの取得元ドメイン</summary>
-        public string Domain;
+        /// <summary>プロセスへのヒモ付情報</summary>
+        public string ProcessKey;
 
         /// <summary>ログの取得元ユーザ</summary>
         public string User;
 
         /// <summary>ログ内容</summary>
         public string LogText;
-
-        /// <summary>WindowHandle</summary>
-        public IntPtr HWnd;
 
         /// <summary>ログのカテゴリ</summary>
         public string Category;
@@ -109,13 +103,97 @@ namespace Neith.Logger.Model
         public string Target;
 
         #endregion
-        #region メソッド
+        #region オブジェクト作成
         public static NeithNotificationRec Create()
         {
             var obj = new NeithNotificationRec();
             obj.ReceptionTime = UniqueTime.UtcNow;
             return obj;
         }
+
+        /// <summary>
+        /// ヘッダオブジェクトよりレコードを作成します。
+        /// </summary>
+        public static NeithNotificationRec FromHeaders(HeaderCollection headers)
+        {
+            var p = headers.GetHeaderStringValue(HeaderKeys.NOTIFICATION_PRIORITY, false);
+            var priority = Neith.Growl.Connector.Priority.Normal;
+            if (p != null) {
+                int pval = 0;
+                bool pok = int.TryParse(p, out pval);
+                if (pok && Enum.IsDefined(typeof(Priority), pval))
+                    priority = (Priority)pval;
+            }
+            var text = headers.GetHeaderStringValue(HeaderKeys.NOTIFICATION_TEXT, false);
+            if (text == null) text = String.Empty;
+
+            var item = new NeithNotificationRec()
+            {
+                Name = headers.GetHeaderStringValue(HeaderKeys.NOTIFICATION_NAME, true),
+                Icon = headers.GetHeaderResourceValue(HeaderKeys.NOTIFICATION_ICON, false),
+
+                ApplicationName = headers.GetHeaderStringValue(HeaderKeys.APPLICATION_NAME, true),
+                ID = headers.GetHeaderStringValue(HeaderKeys.NOTIFICATION_ID, false),
+                CoalescingID = headers.GetHeaderStringValue(HeaderKeys.NOTIFICATION_COALESCING_ID, false),
+                Priority = priority,
+                Sticky = headers.GetHeaderBooleanValue(HeaderKeys.NOTIFICATION_STICKY, false),
+                Title = headers.GetHeaderStringValue(HeaderKeys.NOTIFICATION_TITLE, true),
+                Text = text,
+
+                EventTime = headers.GetHeaderDateTimeOffsetValue(NeithHeaderKeys.EVENT_TIME, false),
+                ProcessKey = headers.GetHeaderStringValue(NeithHeaderKeys.PROCESS_KEY, false),
+                User = headers.GetHeaderStringValue(NeithHeaderKeys.USER, false),
+                LogText = headers.GetHeaderStringValue(NeithHeaderKeys.LOGTEXT, false),
+                Category = headers.GetHeaderStringValue(NeithHeaderKeys.CATEGORY, false),
+                Type = headers.GetHeaderStringValue(NeithHeaderKeys.TYPE, false),
+                Target = headers.GetHeaderStringValue(NeithHeaderKeys.TARGET, false),
+            };
+            item.ToExtensibleObject().SetInhertiedAttributesFromHeaders(headers);
+            return item;
+        }
+
+        #endregion
+        #region 変換
+
+        public IExtensibleObject ToExtensibleObject()
+        {
+            return new MyExtensibleObject(this);
+        }
+
+        /// <summary>基本情報</summary>
+        private class MyExtensibleObject : IExtensibleObject
+        {
+            private readonly NeithNotificationRec Rec;
+            public MyExtensibleObject(NeithNotificationRec rec)
+            {
+                Rec = rec;
+            }
+
+            /// <summary>カスタムバイナリ属性</summary>
+            public Dictionary<string, Resource> CustomBinaryAttributes { get { return Rec.CustomBinaryAttributes; } }
+
+            /// <summary>カスタム文字列属性</summary>
+            public Dictionary<string, string> CustomTextAttributes { get { return Rec.CustomTextAttributes; } }
+
+            /// <summary>マシン名</summary>
+            public string MachineName { get { return Rec.MachineName; } set { Rec.MachineName = value; } }
+
+            /// <summary>プラットフォーム(OS)名</summary>
+            public string PlatformName { get { return Rec.PlatformName; } set { Rec.PlatformName = value; } }
+
+            /// <summary>プラットフォーム(OS)バージョン</summary>
+            public string PlatformVersion { get { return Rec.PlatformVersion; } set { Rec.PlatformVersion = value; } }
+
+            /// <summary>ソフトウェア名</summary>
+            public string SoftwareName { get { return Rec.SoftwareName; } set { Rec.SoftwareName = value; } }
+
+            /// <summary>ソフトウェアバージョン</summary>
+            public string SoftwareVersion { get { return Rec.SoftwareVersion; } set { Rec.SoftwareVersion = value; } }
+        }
+
+
+        #endregion
+        #region 一般メソッド
 
         public override string ToString()
         {
@@ -124,16 +202,6 @@ namespace Neith.Logger.Model
 
         #endregion
 
-        public bool Equals(NeithNotificationRec other)
-        {
-            return this.ReceptionTime == other.ReceptionTime
-                && this.MachineName == other.MachineName
-                && this.Pid == other.Pid
-                && this.SoftwareName == other.SoftwareName
-                && this.Domain == other.Domain
-                && this.User == other.User
-                && this.LogText == other.LogText
-                ;
-        }
     }
+
 }
