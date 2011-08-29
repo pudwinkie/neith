163b5d8ababee7355c9773d314cf7218f92dd859
lucia.neith.vb <lucia.neith.vb@localhost>
2011-08-29 16:30:43 +0900

    ??????????

---
+++ Neith.Signpost2/src/Neith.Logger/Parsers/IPacketReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neith.Logger.Parsers
+{
+    /// <summary>
+    /// パケット読込インターフェース。
+    /// </summary>
+    public interface IPacketReader
+    {
+        /// <summary>
+        /// 指定されたバッファにパケットを読み込み、結果を返します。
+        /// </summary>
+        /// <param name="buffers">バッファ</param>
+        /// <param name="langth">最大読み込みサイズ</param>
+        /// <returns>読み込んだバイト数。</returns>
+        Task<int> ReceiveTaskAsync(IList<ArraySegment<byte>> buffers, int langth);
+    }
+}
