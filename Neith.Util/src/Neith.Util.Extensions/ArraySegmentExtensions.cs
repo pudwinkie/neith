03b41cd1d36d059c71e8a8bc0dbeb486c91cd2e2
lucia.neith.vb <lucia.neith.vb@localhost>
2011-08-30 18:12:44 +0900

    ??????????

--- Neith.Util/src/Neith.Util.Extensions/ArraySegmentExtensions.cs
+++ Neith.Util/src/Neith.Util.Extensions/ArraySegmentExtensions.cs
@@ -28,17 +28,44 @@ namespace System
         }
 
         /// <summary>
+        /// 全てを連結した要素を返します。
+        /// </summary>
+        /// <param name="arrays"></param>
+        /// <returns></returns>
+        public static T[] ToCombineArray<T>(this IEnumerable<T[]> arrays)
+        {
+            var size = arrays.Select(a => a.Length).Sum();
+            var dst = new T[size];
+            var dstOffset = 0;
+            foreach (var src in arrays) {
+                Buffer.BlockCopy(src, 0, dst, dstOffset, src.Length);
+                dstOffset += src.Length;
+            }
+            return dst;
+        }
+
+        /// <summary>
         /// 単純配列にコピーして返します。
         /// </summary>
         /// <param name="src"></param>
         /// <returns></returns>
         public static T[] ToArray<T>(this ArraySegment<T> src)
         {
-            var dst = new T[src.Count];
-            Buffer.BlockCopy(src.Array, src.Offset, dst, 0, src.Count);
-            return dst;
+            return src.ToArray(src.Count);
         }
 
+        /// <summary>
+        /// 単純配列にコピーして返します。
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public static T[] ToArray<T>(this ArraySegment<T> src, int count)
+        {
+            var dst = new T[count];
+            Buffer.BlockCopy(src.Array, src.Offset, dst, 0, count);
+            return dst;
+        }
 
     }
 }
