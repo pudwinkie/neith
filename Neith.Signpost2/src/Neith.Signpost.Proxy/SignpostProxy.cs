dcc37592e2db984ded6c459b23b84296d656eeca
lucia.neith.vb <lucia.neith.vb@localhost>
2011-09-09 18:17:09 +0900

    ??

---
+++ Neith.Signpost2/src/Neith.Signpost.Proxy/SignpostProxy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Neith.Signpost.Proxy
+{
+    [Serializable]
+    public class SignpostProxy : MarshalByRefObject, ISignpostServer
+    {
+        /// <summary>
+        /// リモート通信チャネルを取得又は設定します。
+        /// </summary>
+        public TcpServerChannel Channel { get { return _Channel; } internal set { _Channel = value; } }
+        [NonSerialized]
+        private TcpServerChannel _Channel;
+
+
+
+        /// <summary>
+        /// クライアント通信が生きているならtrueを返します。
+        /// 死んでいる場合は例外、あるいはfalseを返します。
+        /// </summary>
+        /// <returns>常にtrueを返す。</returns>
+        public bool IsAlive()
+        {
+            return true;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
