c1314545ae6a6444934bf843024fa4f62d391119
lucia.neith.vb <lucia.neith.vb@localhost>
2011-09-15 16:36:04 +0900

    ?????????????

---
+++ Neith.Signpost2/src/Neith.Interop/Win32/KeyboardAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Neith.Interop.Win32
+{
+    public static class KeyboardAction
+    {
+        public static void PostDown(IntPtr hWnd, Key key)
+        {
+            var vKey = KeyInterop.VirtualKeyFromKey(key);
+            NativeMethods.PostMessage(hWnd, WindowMessage.KeyDown, (IntPtr)vKey, IntPtr.Zero);
+        }
+
+        public static void PostUp(IntPtr hWnd, Key key)
+        {
+            var vKey = KeyInterop.VirtualKeyFromKey(key);
+            NativeMethods.PostMessage(hWnd, WindowMessage.KeyUp, (IntPtr)vKey, IntPtr.Zero);
+        }
+
+
+    }
+}
---
+++ Neith.Signpost2/src/Neith.Signpost.Proxy/ISignpostService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Neith.Signpost.Proxy
+{
+    /// <summary>
+    /// Signpostアプリ間の基本サービス。
+    /// </summary>
+    public interface ISignpostBaseService
+    {
+        /// <summary>
+        /// サービス開始を通知します。
+        /// </summary>
+        /// <param name="name"></param>
+        [OperationContract(IsOneWay = true)]
+        void Join(string name);
+
+        /// <summary>
+        /// サービス終了を通知します。
+        /// </summary>
+        /// <param name="name"></param>
+        [OperationContract(IsOneWay = true)]
+        void Leave(string name);
+
+        /// <summary>
+        /// 全てのサービスに応答を要求します。
+        /// </summary>
+        /// <param name="name"></param>
+        [OperationContract(IsOneWay = true)]
+        void RequestEcho(string name);
+
+        /// <summary>
+        /// 応答します。
+        /// </summary>
+        /// <param name="name"></param>
+        [OperationContract(IsOneWay = true)]
+        void ResponseEcho(string name);
+
+    }
+
+
+    /// <summary>
+    /// サーバ側状態の購読サービス。
+    /// </summary>
+    public interface ISignpostSubscriptionService
+    {
+
+
+    }
+
+
+    /// <summary>
+    /// サーバへのアクションサービス。
+    /// </summary>
+    public interface ISignpostActionService
+    {
+
+
+    }
+
+
+
+
+    [ServiceContract(Namespace = "http://signpost.vbel.net/peerservice",
+    CallbackContract = typeof(ISignpostService))]
+    public interface ISignpostService : ISignpostBaseService, ISignpostSubscriptionService, ISignpostActionService
+    {
+    }
+
+    public interface ISignpostServiceChannel : ISignpostService, IClientChannel
+    {
+    }
+}
