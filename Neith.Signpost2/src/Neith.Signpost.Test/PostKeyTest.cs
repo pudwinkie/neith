34b1eb874aa9d4a29bdc38e0623ef05458e56db4
lucia.neith.vb <lucia.neith.vb@localhost>
2011-09-16 17:50:01 +0900

    UnitTest

---
+++ Neith.Signpost2/src/Neith.Signpost.Test/PostKeyTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reactive.Linq;
+using System.Reactive.Concurrency;
+using Neith.Interop.Win32;
+
+namespace Neith.Signpost.Test
+{
+    using NUnit.Framework;
+    public class PostKeyTest : AssertionHelper
+    {
+        //[Test]
+        public void TestMethod1()
+        {
+            'F'.SendShift();
+            var text = "ゆにこーどしけん\r\n";
+            Console.Clear();
+            var oRead = Observable.Start(() => Console.ReadLine(), Scheduler.NewThread);
+            var oInput = Observable.Start(() => text.SendInput(), Scheduler.NewThread);
+            using (oInput.Subscribe()) {
+                var rc = oRead.First();
+                Expect(rc, Is.SameAs(text));
+            }
+        }
+    }
+}
---
+++ Neith.Signpost2/src/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyInfo")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("436d7a8d-4704-4486-b5a6-57b37d9bb105")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
