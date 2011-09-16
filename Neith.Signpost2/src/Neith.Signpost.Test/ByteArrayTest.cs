e00ed73f8d121e9a9e85d459313202e800f2942b
lucia.neith.vb <lucia.neith.vb@localhost>
2011-09-16 17:50:01 +0900

    UnitTest

---
+++ Neith.Signpost2/src/Neith.Signpost.Test/ByteArrayTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neith.Signpost.Test
+{
+    using NUnit.Framework;
+    [TestFixture]
+    public class ByteArrayTest : AssertionHelper
+    {
+        [Test]
+        public void TestByteArray()
+        {
+            var src = Enumerable.Range(0, 256).Select(a => (byte)a).ToArray();
+            for (var i = 0; i < 256; i++) {
+                var a = SubArray1(src, i);
+                var b = SubArray2(src, i);
+                Expect(a.SequenceEqual(b));
+            }
+        }
+
+        public static byte[] SubArray1(byte[] src, int length)
+        {
+            var dst = new byte[length];
+            Array.Copy(src, dst, length);
+            return dst;
+        }
+        public static byte[] SubArray2(byte[] src, int length)
+        {
+            return src.Take(length).ToArray();
+        }
+
+    }
+}
---
+++ Neith.Signpost2/src/UnitTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace UnitTest
+{
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return Run(args);
+        }
+
+        internal static int Run(string[] args)
+        {
+            List<string> p = new List<string>();
+            // [*.Test.dll]な名前のファイルをすべて追加
+            foreach (string path in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.test.dll")) {
+                p.Add(path);
+            }
+            p.Add("/nologo");
+            p.Add("/nodots");
+            bool failStop = false;
+            foreach (string arg in args) {
+                if (arg == "/failstop") {
+                    failStop = true;
+                    continue;
+                }
+                p.Add(arg);
+            }
+
+            //ConsoleUi test = new ConsoleUi();
+            //int rc = test.Execute(new ConsoleOptions(p.ToArray()));
+            int rc = NUnit.ConsoleRunner.Runner.Main(p.ToArray());
+            if (rc != 0) {
+                if (failStop) {
+                    Console.WriteLine("ユニットテストに失敗しました。何かKeyを押してください。");
+                    Console.ReadKey();
+                }
+            }
+            return rc;
+        }
+
+    }
+}
