d38de119a044bd0fe34fde4b97af637c4a67f77c
lucia.neith.vb <lucia.neith.vb@localhost>
2011-09-22 18:19:56 +0900

    ???

---
+++ Neith.Signpost3/src/Neith.Signpost.SL/Views/Home.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Neith.Signpost.Services;
+
+namespace Neith.Signpost.SL
+{
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        // ユーザーがこのページに移動したときに実行されます。
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void ContentText_Loaded(object sender, RoutedEventArgs e)
+        {
+            var ctrl = sender as TextBlock;
+            try {
+                var sv = new SignpostContext();
+                sv.GetServerTime(rc =>
+                {
+                    if (rc.IsComplete) ctrl.Text = string.Format("サーバ時刻={0}", rc.Value);
+                    if (rc.HasError) {
+                        ctrl.Text = rc.Error.ToString();
+                        rc.MarkErrorAsHandled();
+                    }
+                }, null);
+            }
+            catch (Exception ex) {
+                ctrl.Text = ex.ToString();
+            }
+        }
+    }
+}
---
+++ Neith.Signpost3/src/Neith.Signpost/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.DomainServices.Hosting;
+using Neith.Signpost.Services;
+
+namespace Neith.Signpost
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        public List<ServiceHost> Hosts { get; private set; }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            TaskEx.Run(StartupCode);
+        }
+
+        /// <summary>
+        /// 非同期スタートアップコードの実行。
+        /// </summary>
+        private void StartupCode()
+        {
+            var uri = new Uri("http://localhost:1414/");
+            Hosts = new List<ServiceHost>();
+            Hosts.Add(new DomainServiceHost(typeof(SignpostService), uri));
+        }
+
+    }
+}
