890bfc56ac0e238ec042b03ddc84f673eff61603
lucia.neith.vb <lucia.neith.vb@localhost>
2011-09-22 18:19:56 +0900

    ???

---
+++ Neith.Signpost3/src/Neith.Signpost.SL/Views/About.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Neith.Signpost.SL
+{
+    public partial class About : Page
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        // ユーザーがこのページに移動したときに実行されます。
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
---
+++ Neith.Signpost3/src/Neith.Signpost.Services.Web/Services/SignpostService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.ServiceModel.DomainServices.Hosting;
+using System.ServiceModel.DomainServices.Server;
+
+namespace Neith.Signpost.Services
+{
+    // TODO: アプリケーション ロジックを含むメソッドを作成します。
+    [EnableClientAccess()]
+    public class SignpostService : DomainService
+    {
+        [Invoke]
+        public DateTimeOffset GetServerTime()
+        {
+            return DateTimeOffset.Now;
+        }
+
+    }
+}
