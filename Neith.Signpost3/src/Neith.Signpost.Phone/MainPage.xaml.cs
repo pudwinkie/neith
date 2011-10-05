d46f24deec370a6d3c2520abaa445d0a0bd987ee
lucia.neith.vb <lucia.neith.vb@localhost>
2011-10-05 18:08:34 +0900

    Phone?????????????

---
+++ Neith.Signpost3/src/Neith.Signpost.Phone/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Neith.Signpost
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // ListBox コントロールのデータ コンテキストをサンプル データに設定します
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        // ViewModel Items のデータを読み込みます
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded) {
+                App.ViewModel.LoadData();
+            }
+        }
+    }
+}
