22807f28aa4b6aa2ff034db51be58131c0fdb3e6
lucia.neith.vb <lucia.neith.vb@localhost>
2011-10-05 17:24:28 +0900

    ????????????????

--- Neith.Signpost3/src/Neith.Signpost.SL/Views/About.xaml.cs
+++ Neith.Signpost3/src/Neith.Signpost.SL/Views/About.xaml.cs
@@ -24,5 +24,49 @@ namespace Neith.Signpost
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
         }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs ev)
+        {
+            Dispatcher.BeginInvoke(() => TileGridSizeChange(ev));
+
+        }
+
+
+        private int lastColumnCount = -1;
+        private int lastRowCount = -1;
+
+        private const double BASE_SIZE = 120;
+        private const double SPLIT_SIZE = 12;
+        private const double SPAN_SIZE = BASE_SIZE + SPLIT_SIZE;
+        private static readonly GridLength GL_ZERO = new GridLength(0);
+        private static readonly GridLength GL_SPAN = new GridLength(SPAN_SIZE);
+
+        private void TileGridSizeChange(SizeChangedEventArgs ev)
+        {
+            var size = ev.NewSize;
+            var columnCount = (int)Math.Floor(size.Width / SPAN_SIZE);
+            var rowCount = (int)Math.Floor(size.Height / SPAN_SIZE);
+            if (columnCount > 16) columnCount = 16;
+            if (rowCount > 16) rowCount = 16;
+            if (lastColumnCount == columnCount && lastRowCount == rowCount) return;
+
+            lastColumnCount = columnCount;
+            lastRowCount = rowCount;
+            tbSize.Text = string.Format("size: (row,col)=({0},{1})", rowCount, columnCount);
+
+
+            TileGrid.Width = SPAN_SIZE * columnCount;
+            foreach (var item in TileGrid.ColumnDefinitions) {
+                item.Width = columnCount > 0 ? GL_SPAN : GL_ZERO;
+                columnCount--;
+            }
+
+            TileGrid.Height = SPAN_SIZE * rowCount;
+            foreach (var item in TileGrid.RowDefinitions) {
+                item.Height = rowCount > 0 ? GL_SPAN : GL_ZERO;
+                rowCount--;
+            }
+
+        }
     }
 }
