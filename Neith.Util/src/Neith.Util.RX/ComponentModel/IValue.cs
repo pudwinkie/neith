cb2594f0a8074a6b57fce9ca511907c49b792909
lucia.neith.vb <lucia.neith.vb@localhost>
2011-10-14 15:28:31 +0900

    Rx???

---
+++ Neith.Util/src/Neith.Util.RX/ComponentModel/IValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neith.ComponentModel
+{
+    public interface IValue
+    {
+        object Value { get; set; }
+    }
+
+}
