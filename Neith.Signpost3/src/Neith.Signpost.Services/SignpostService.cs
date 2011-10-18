c0aeab14c9c7c2d3bc23435036896523089c9a30
lucia.neith.vb <lucia.neith.vb@localhost>
2011-10-18 14:46:00 +0900

    ????????? ??????????

--- Neith.Signpost3/src/Neith.Signpost.Services/SignpostService.cs
+++ Neith.Signpost3/src/Neith.Signpost.Services/SignpostService.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.Diagnostics;
 using System.ServiceModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Neith.Signpost.Services
 {
@@ -19,6 +20,27 @@ namespace Neith.Signpost.Services
             Debug.WriteLine("[SignpostService::GetServerTime] " + now);
             return now;
         }
+        private static readonly TaskFactory tf = new TaskFactory();
+
+        public IAsyncResult BeginGetServerTime(AsyncCallback callback, object state)
+        {
+            var t1 = new Task<DateTimeOffset>(st => GetServerTime(), state);
+            t1.Start();
+            var t2 = tf.ContinueWhenAll(new[] { t1 }, a => callback(t1));
+            return t1;
+        }
+
+        public DateTimeOffset EndGetServerTime(IAsyncResult result)
+        {
+            var task = result as Task<DateTimeOffset>;
+            return task.Result;
+        }
+
+
+
+
+
+
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
         {
