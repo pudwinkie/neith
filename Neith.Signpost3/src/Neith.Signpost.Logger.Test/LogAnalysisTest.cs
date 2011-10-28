1a8de569f8bf0f3efabf5845243ecf8a3c7a56c6
lucia.neith.vb <lucia.neith.vb@localhost>
2011-10-28 15:18:06 +0900

    ?????log????

---
+++ Neith.Signpost3/src/Neith.Signpost.Logger.Test/LogAnalysisTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Diagnostics;
+
+namespace Neith.Signpost.Logger.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LogAnalysisTest
+    {
+        private static Stream GetLogStream()
+        {
+            var log = File.OpenRead(Const.XmlLogPath);
+            var mem = new MemoryStream(Encoding.UTF8.GetBytes("</body></html>"));
+            return new CombineReadStream(log, mem);
+        }
+
+
+
+
+        [Test]
+        public void ReadTest()
+        {
+            var setting = new XmlReaderSettings
+            {
+                DtdProcessing = System.Xml.DtdProcessing.Parse,
+                IgnoreComments = true,
+
+            };
+            using (var st = GetLogStream())
+            using (var reader = XmlReader.Create(st, setting)) {
+                var doc = XDocument.Load(reader);
+                var logCount = doc.Descendants("p").Count();
+                Debug.WriteLine(string.Format("log count={0}", logCount));
+            }
+        }
+
+
+    }
+}
---
+++ Neith.Util/src/Neith.Util.RX/Windows/ResourceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+
+namespace System.Windows
+{
+    public static class ResourceExtensions
+    {
+        /// <summary>
+        /// プロパティにダイナミックリソースを割り当てます。
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="dp"></param>
+        /// <param name="key"></param>
+        public static void SetDynamicResource(this DependencyObject target, DependencyProperty dp, object key)
+        {
+            var src = new ValueTarget(target, dp);
+            var dre = new DynamicResourceExtension(key);
+            target.SetValue(dp, dre.ProvideValue(src));
+        }
+        internal class ValueTarget : IServiceProvider, IProvideValueTarget
+        {
+            internal ValueTarget(DependencyObject target, DependencyProperty dp)
+            {
+                TargetObject = target;
+                TargetProperty = dp;
+            }
+
+            public object TargetObject { get; private set; }
+            public object TargetProperty { get; private set; }
+            public object GetService(Type serviceType)
+            {
+                return (serviceType == typeof(IProvideValueTarget)) ? this : null;
+            }
+        }
+    }
+}
