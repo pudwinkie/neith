3801177582b8f2f842829bb465f16b890e16f37d
lucia.neith.vb <lucia.neith.vb@localhost>
2011-10-28 16:00:26 +0900

    MS SgmlReader

---
+++ Neith.Signpost3/src/Neith.Signpost.Logger/XN.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Neith.Signpost.Logger
+{
+    public static class XN
+    {
+        public static readonly XName p = "p";
+        public static readonly XName b = "b";
+        public static readonly XName li = "li";
+        public static readonly XName ul = "ul";
+        public static readonly XName div = "div";
+        public static readonly XName span = "span";
+        public static readonly XName datetime = "datetime";
+        public static readonly XName time = "time";
+
+        public static readonly XName itemscope = "itemscope";
+        public static readonly XName itemprop = "itemprop";
+
+    }
+}
